8edef6f5821af3a4f1e29c625d73ffceb92ff4ef
George Kenny <george.kenny2@googlemail.com>
2024-08-10 17:21:33 +0100

    Add multiplayer support

--- characters/youtubetutorialplayer/youtube_player.gd
+++ characters/youtubetutorialplayer/youtube_player.gd
@@ -4,22 +4,28 @@ signal health_depleted
 
 var health = 100.0
 
+func _enter_tree():
+	set_multiplayer_authority(name.to_int())
+
 func _physics_process(delta):
-	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	
-	velocity = direction * 800
+	if is_multiplayer_authority():
+		var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+		velocity = direction * 800
+		if velocity.length() > 0.0:
+			%HappyBoo.play_walk_animation()
+		else:
+			%HappyBoo.play_idle_animation()
+		
+		
+		const DAMAGE_RATE = 20.0
+		var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+		if overlapping_mobs.size() > 0:
+			health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+			%HealthBar.value = health
+			if health <= 0.0:
+				queue_free()
+				health_depleted.emit()
+
 	move_and_slide()
-	
-	if velocity.length() > 0.0:
-		%HappyBoo.play_walk_animation()
-	else:
-		%HappyBoo.play_idle_animation()
-	
-	const DAMAGE_RATE = 20.0
-	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
-	if overlapping_mobs.size() > 0:
-		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
-		%HealthBar.value = health
-		if health <= 0.0:
-			queue_free()
-			health_depleted.emit()
+		
+
