d105fded8e4df9ab276496337998b9f41bfc1725
George Kenny <george.kenny2@googlemail.com>
2024-08-10 17:36:36 +0100

    Fix multiplayer

--- levels/level1_multiplayer.gd
+++ levels/level1_multiplayer.gd
@@ -10,7 +10,7 @@ func _ready():
 
 
 func _on_host_pressed():
-	peer.create_server(135)
+	peer.create_server(9090)
 	multiplayer.multiplayer_peer = peer
 	multiplayer.peer_connected.connect(_add_player)
 	_add_player()
@@ -22,5 +22,5 @@ func _add_player(id = 1):
 	call_deferred("add_child", player)
 
 func _on_join_pressed():
-	peer.create_client("localhost", 135)
+	peer.create_client("localhost", 9090)
 	multiplayer.multiplayer_peer = peer
