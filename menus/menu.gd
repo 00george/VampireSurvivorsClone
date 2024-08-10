453e6d33258452cd18283d13d91110388d0ebd54
Ivan <ivandankov7@gmail.com>
2024-08-10 14:25:52 +0100

    Add start menu (#4)
    * Add main menu
    
    * Link menu to first level
    
    ---------
    
    Co-authored-by: Seven7een <0x86239@protonmail.com>

---
+++ menus/menu.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_start_pressed():
+	print("Start pressed") # Replace with function body.
+	# TODO: Change to real path for first level/scene
+	get_tree().change_scene_to_file("res://levels/Level1.tscn")
+
+func _on_options_pressed():
+	print("Options pressed")
+	get_tree().change_scene_to_file("res://menus/options.tscn")
+
+func _on_exit_pressed():
+	get_tree().quit()
