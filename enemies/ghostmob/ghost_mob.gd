f7c07e8e89436465eb8a29c2af11293da7015483
tae-ookchung <45942459+tae-ookchung@users.noreply.github.com>
2024-08-10 18:29:00 +0100

    added ghost ai, health and dmg (#14)

--- enemies/ghostmob/ghost_mob.gd
+++ enemies/ghostmob/ghost_mob.gd
@@ -1,21 +1,21 @@
 extends CharacterBody2D
 
-var health = 5
+var health = 3
+
+@export var speed: int = 600.0
 
 var player
 
 func _ready():
 	player = get_tree().get_first_node_in_group("PlayersGroup")
-	$Slime.play_walk()
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 150.0
+	velocity = direction * speed
 	move_and_slide()
 
 func take_damage():
 	health -= 1
-	$Slime.play_hurt()
 	
 	if health == 0:
 		queue_free()
