66f23226a0aa8b590fe51bd35ec033775ae31d67
Gavin H <gavinhoang@gmail.com>
2024-08-11 14:40:53 +0100

    Created player stats

--- characters/player_base/player_health.gd
+++ characters/player_base/player_health.gd
@@ -7,6 +7,8 @@ signal health_depleted
 @export var main_menu_scene: PackedScene
 @export var health_bar: ProgressBar
 @export var hurt_box: Area2D
+@export var player_stats: PlayerStats
+
 
 func _ready():
 	pass
@@ -14,11 +16,12 @@ func _physics_process(delta):
 	const DAMAGE_RATE = 20.0
 	var overlapping_mobs = hurt_box.get_overlapping_bodies()
 	if overlapping_mobs.size() > 0:
-		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		var damage_multiplier = 2 / (player_stats.defence + 1)
+		health -= (DAMAGE_RATE * overlapping_mobs.size() * delta) * damage_multiplier
 		health_bar.value = health
 		if health <= 0.0:
 			get_tree().change_scene_to_packed(main_menu_scene)
-			health_depleted.emit()		
+			health_depleted.emit()
 
 func change_health(amount):
 	health += amount
