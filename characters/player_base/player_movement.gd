4862e4dfccd909f8db0127b9ac1d08cd7abba4c0
Gavin H <gavinhoang@gmail.com>
2024-08-11 14:40:53 +0100

    Created player stats

--- characters/player_base/player_movement.gd
+++ characters/player_base/player_movement.gd
@@ -2,7 +2,8 @@ extends Node
 
 @export var animatedSprite: AnimatedSprite2D;
 @export var characterBody2D: CharacterBody2D;
-@export var speed: int = 200;
+@export var player_stats: PlayerStats
+var speed: int = 200;
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -21,4 +22,4 @@ func _physics_process(delta):
 	if direction == Vector2.ZERO:
 		animatedSprite.play("idle_down")
 		
-	characterBody2D.move_and_collide(direction * speed * delta)
+	characterBody2D.move_and_collide(direction * speed * player_stats.speed * delta)
---
+++ characters/player_base/player_stats.gd
@@ -0,0 +1,6 @@
+class_name PlayerStats extends Resource
+
+@export var attack: float = 1.0
+@export var defence: float = 1.0
+@export var speed: float = 1.0
+@export var xp_gain: float = 1.0
